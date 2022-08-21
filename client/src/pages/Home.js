46ac693c109bd32ffed2900f16d29662c09a8a92
Beimy <arbm159357@gmail.com>
2022-08-21 14:04:15 -0400

    rendering with react router

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <Fragment>
+            <h1>Hello There</h1>
+        </Fragment>
+    )
+};
+
+export default Home;
